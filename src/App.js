da7980c6fac5b9f2e9fdd64dcc6861893fffc42e
dobbi_free <gangb6444@gmail.com>
2021-07-13 17:44:58 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Button, Grid, Typography } from "@material-ui/core";
+import {interval, Observable, scan, startWith, takeUntil, takeWhile} from "rxjs";
+const stream$ = interval(1000)
 function App() {
+  const [time, setTime] = useState(0);
+  const [timerActive, setTimerActive] = useState(false);
+
+
+  const start = () => {
+    setTimerActive(true)
+
+  };
+
+  const stop = () => {
+    setTimerActive(false)
+  };
+
+  const resetTimer = () => {
+    setTime(0)
+  };
+
+  useEffect(() => {
+    if (timerActive) {
+      const sub = stream$.pipe(
+          startWith(time),
+          scan(value => value + 1),
+      ).subscribe(setTime)
+      return () => sub.unsubscribe();
+    }
+  }, [timerActive,time]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant={"h1"}>
+          {("0" + Math.floor((time / 21600) % 60)).slice(-2)}:
+        </Typography>
+        <Typography variant={"h1"}>
+          {("0" + Math.floor((time / 60) % 60)).slice(-2)}:
+        </Typography>
+        <Typography variant={"h1"}>{("0" + (time % 60)).slice(-2)}</Typography>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        xs={6}
+      >
+        <Grid container justifyContent="center" alignItems="center" xs={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={start}
+          >
+            Start
+          </Button>
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center" xs={2}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onDoubleClick={stop}
+          >
+            Stop
+          </Button>
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center" xs={2}>
+          <Button variant="contained" onClick={resetTimer}>
+            Reset
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
